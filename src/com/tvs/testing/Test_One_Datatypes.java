bcbbf373ae750175c355fa6e8de1a6126f5cc21a
Ska2247 <karthick.s.2909@gmail.com>
2023-01-22 06:59:01 +0530

    Test_One_Datatypes

---
+++ src/com/tvs/testing/Test_One_Datatypes.java
@@ -0,0 +1,26 @@
+package com.tvs.testing;
+
+public class Test_One_Datatypes {
+
+	public static void main(String[] args) {
+
+		byte a = 45;
+		System.out.println("the value of byte = " + a);
+		short b = 7896;
+		System.out.println("the value of short = " + b);
+		int c = 56785498;
+		System.out.println("the value of int = " + c);
+		long d = 123456789012345l;
+		System.out.println("the value of long = " + d);
+		float e = 0.1234567f;
+		System.out.println("the value of float = " + e);
+		double f = 0.12345678909876;
+		System.out.println("the value of double = " + f);
+		char g = '#';
+		System.out.println("the value of char = " + g);
+		boolean h = true;
+		System.out.println("the value of boolean = " + h);
+		String i = "cute cat sitting on the table";
+		System.out.println("the value of String = " + i);
+	}
+}
